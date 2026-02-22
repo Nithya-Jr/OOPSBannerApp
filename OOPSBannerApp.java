bf0a37df29a5dd0d512e267f54a45c8ab984afa2
Nithya-Jr <nj1886@srmist.edu.in>
2026-02-22 19:37:50 +0530

    UC3: String.join implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,14 @@
 /**
  * @author: Nithzrm -f *.class
- * @version: 1.0
+ * @version: 1.1
  */
-
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(" **     **    **     ***  ");
-        System.out.println("*     *   *     *   *     *   *     * ");
-        System.out.println("*     *   *     *   *     *   *       ");
-        System.out.println("*     *   *     *   **     ***  ");
-        System.out.println("*     *   *     *   *              *  ");
-        System.out.println("*     *   *     *   *        *     *  ");
-        System.out.println(" **     **    *         ***   ");
-
+        System.out.println(String.join(" ", " * ", " * ", " ** ", " ** "));
+        System.out.println(String.join(" ", "*   ", "   ", "     ", "*     "));
+        System.out.println(String.join(" ", "*   ", "   ", "*  ", "**  "));
+        System.out.println(String.join(" ", "*   ", "   ", "     ", "    * "));
+        System.out.println(String.join(" ", " * ", " * ", "*     ", "**  "));
     }
 }
